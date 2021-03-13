517bd1173348434d9abb9b9c0b7aa33d2e00ce13
Alexander <akalvachev99@gmail.com>
2021-03-13 18:17:32 +0200

    Fix empty favourites page

--- assets/js/favouritesController.js
+++ assets/js/favouritesController.js
@@ -1,29 +1,44 @@
 function printFavourites() {
     let currentUserFavs = userService.getCurrentUser().favourites;
 
-    // if (currentUserFavs.length === 0) {
-    //     let message = document.createElement('img');
-    //     message.src = './assets/images/message-booking.png';
-    //     message.style.width = '1500px';
-    //     message.className = 'emptyFavMessage';
-    //     FAVOURITES_PAGE.append(message);
-    // }
-
-    currentUserFavs.forEach(hotel => {
-        printAllHotels(hotel, FAVOURITES_PAGE);
-    })
-}
-
-PROFILE_BUTTON.addEventListener('click', function () {
-    let currentUserFavs = userService.getCurrentUser().favourites;
     if (currentUserFavs.length !== 0) {
         FAVOURITES_PAGE.innerHTML = '';
+
+        currentUserFavs.forEach(hotel => {
+            printAllHotels(hotel, FAVOURITES_PAGE);
+        })
     } else if (currentUserFavs.length === 0) {
         FAVOURITES_PAGE.innerHTML = '';
+
+        let linkMessage = document.createElement('a');
+        linkMessage.href = '#home';
+
         let message = document.createElement('img');
         message.src = './assets/images/message-booking.png';
-        message.style.width = '1500px';
+        message.style.width = '815px';
         message.className = 'emptyFavMessage';
-        FAVOURITES_PAGE.append(message);
+
+        linkMessage.append(message);
+
+        FAVOURITES_PAGE.append(linkMessage);
     }
-})
+}
+
+// PROFILE_BUTTON.addEventListener('click', function () {
+//     let currentUserFavs = userService.getCurrentUser().favourites;
+
+//     if (currentUserFavs.length !== 0) {
+//         FAVOURITES_PAGE.innerHTML = '';
+//         currentUserFavs.forEach(hotel => {
+//             printAllHotels(hotel, FAVOURITES_PAGE);
+//         })
+//     } else if (currentUserFavs.length === 0) {
+//         FAVOURITES_PAGE.innerHTML = '';
+
+//         let message = document.createElement('img');
+//         message.src = './assets/images/message-booking.png';
+//         message.style.width = '1500px';
+//         message.className = 'emptyFavMessage';
+//         FAVOURITES_PAGE.append(message);
+//     }
+// })
