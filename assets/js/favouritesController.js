a46aa57d824c6fa37dd1f5a6af64924b8b4a4001
Kalin Roussinov <kalinrouss@gmail.com>
2021-03-13 16:57:32 +0200

    added message when there is no favourites

--- assets/js/favouritesController.js
+++ assets/js/favouritesController.js
@@ -1,10 +1,29 @@
 function printFavourites() {
     let currentUserFavs = userService.getCurrentUser().favourites;
+
+    // if (currentUserFavs.length === 0) {
+    //     let message = document.createElement('img');
+    //     message.src = './assets/images/message-booking.png';
+    //     message.style.width = '1500px';
+    //     message.className = 'emptyFavMessage';
+    //     FAVOURITES_PAGE.append(message);
+    // }
+
     currentUserFavs.forEach(hotel => {
         printAllHotels(hotel, FAVOURITES_PAGE);
     })
 }
 
 PROFILE_BUTTON.addEventListener('click', function () {
-    FAVOURITES_PAGE.innerHTML = '';
+    let currentUserFavs = userService.getCurrentUser().favourites;
+    if (currentUserFavs.length !== 0) {
+        FAVOURITES_PAGE.innerHTML = '';
+    } else if (currentUserFavs.length === 0) {
+        FAVOURITES_PAGE.innerHTML = '';
+        let message = document.createElement('img');
+        message.src = './assets/images/message-booking.png';
+        message.style.width = '1500px';
+        message.className = 'emptyFavMessage';
+        FAVOURITES_PAGE.append(message);
+    }
 })
