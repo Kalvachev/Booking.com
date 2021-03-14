57cff852e14e39e262610b898409af776e564e05
Alexander <akalvachev99@gmail.com>
2021-03-14 14:12:47 +0200

    Work on login and register

--- assets/js/loginController.js
+++ assets/js/loginController.js
@@ -23,9 +23,9 @@ MODAL_CLOSE_REGISTER.addEventListener('click', function () {
 //     MODAL_BG_REGISTER_HOME.classList.remove('bg-active');
 // })
 
-GO_TO_REGISTER_BTN.addEventListener('click', function () {
-    MODAL_BG_REGISTER.classList.add('bg-active');
-})
+// GO_TO_REGISTER_BTN.addEventListener('click', function () {
+//     MODAL_BG_REGISTER.classList.add('bg-active');
+// })
 
 SUBMIT_BTN_REG.addEventListener('click', function (ev) {
     ev.preventDefault();
@@ -35,13 +35,24 @@ SUBMIT_BTN_REG.addEventListener('click', function (ev) {
     const repeatedPass = REPEAT_PASSWORD_INPUT.value;
     const email = EMAIL_INPUT_REG.value;
 
-    if (password === repeatedPass) {
+    if (username !== '' && username.length >= 7 && password === repeatedPass && email !== '' && password !== '' && password.length >= 7) {
         userService.register(username, password, email);
-    } else {
-        alert('Passwords dose not match');
+        MODAL_BG_REGISTER.classList.remove('bg-active');
+    } else if (username === '') {
+        alert('Username is required');
+    } else if (username.length < 7) {
+        alert('Username must be at least 7 characters');
+    } else if (email === '') {
+        alert('Email is required');
+    } else if (password === '') {
+        alert('Password is required');
+    } else if (password.length < 7) {
+        alert('Password must be at least 7 characters');
+    } else if (password !== repeatedPass) {
+        alert('Passwords does not match');
+
     }
 
-    MODAL_BG_REGISTER.classList.remove('bg-active');
 });
 
 LOGIN_BTN_MODAL.addEventListener('click', function (ev) {
@@ -51,7 +62,7 @@ LOGIN_BTN_MODAL.addEventListener('click', function (ev) {
     const password = PASSWORD_INPUT.value;
 
     if (userService.login(username, password)) {
-        window.location.hash = `#${username}`;
+        window.location.hash = `#home`;
         MODAL_BG_LOGIN.classList.remove('bg-active');
 
         USERNAME_INPUT.value = '';
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -53,3 +53,8 @@ function changeFavouritesIcon(x) {
         x.classList.add("fas fa-heart");
     }
 }
+
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
